f77ff2ab52d6bb707e2c197581094d556b25ee26
balazs.koncz <balazs.koncz.developer@gmail.com>
2020-10-06 22:29:20 +0200

    updated connection strings

---
+++ PrimeCalculator/Helpers/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeCalculator.Helpers
+{
+    public class ConnectionStrings
+    {
+        public string DatabaseConnection { get; set; }
+
+        public string ScienceConnection { get; set; }
+    }
+}
